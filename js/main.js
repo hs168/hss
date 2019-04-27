2dfca5cf02662b107db4e9af2fa358858cca2bc6
hs168 <50037496+hs168@users.noreply.github.com>
2019-04-27 18:43:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Harini Sridhar.", "An Artist.", "A Writer.", "A Teacher."], //"A Teacher"],//
+      strings: ["Harini Sridhar.", "An Artist.", "A Writer.", "A Dancer.", "A Researcher."], //"A Teacher"],//
       typeSpeed: 100,
       loop: true,
     });
